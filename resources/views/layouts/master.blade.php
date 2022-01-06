10bb81335ee0d4f1ef0aca771c7814f4bfe3933e
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2022-01-06 12:06:09 +0900

    css 분리

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/reset.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+    @yield('css')
     <title>@yield('title','나의 개발 유형찾기')</title>
 </head>
 <body>
