f5d8016614588dcccdec0298db399797c1accc10
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2022-01-18 17:52:07 +0900

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,34 +1,87 @@
 @extends('layouts.master')
 @section('css')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/form.css')}}">
 @endsection
 @section('title')
     개발 유형 설문지
 @endsection
 @section('contents')
-    <section class="survey">
+    <section id="survey">
         <div class="wrapper">
             <form id="form" action="">
-                <div class="question_container">
-                    <h3 class="number">1/-</h3>
-                    <h3 class="question">당신이 가장 재밌었던 수업은?</h3>
+                <div class="test">
+                    <div class="question_container">
+                        <h3 class="number">1/-</h3>
+                        <h3 class="question">당신이 가장 재밌었던 수업은?</h3>
+                    </div>
+                    <div class="answer">
+                        <div> <!-- div 1개에 질문 1개 -->
+                            <!-- input 과 label을 같이 가져갑니다 -->
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">게임 개발 유니티</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">어쩌구 개발</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">저쩌구 개발발</label>
+                        </div>
+                    </div>
+                    <div class="btn_wrap">
+                        <button class="next_btn">다 음</button>
+                    </div>
                 </div>
-                <div class="answer">
-                    <div> <!-- div 1개에 질문 1개 -->
-                        <!-- input 과 label을 같이 가져갑니다 -->
-                        <input id="one" type="radio" name="answer_1">
-                        <label for="one">게임 개발 유니티</label>
+                <div class="test">
+                    <div class="question_container">
+                        <h3 class="number">2/-</h3>
+                        <h3 class="question">당신이 가장 재밌었던 수업은?</h3>
                     </div>
-                    <div>
-                        <input id="one" type="radio" name="answer_1">
-                        <label for="one">어쩌구 개발</label>
+                    <div class="answer">
+                        <div> <!-- div 1개에 질문 1개 -->
+                            <!-- input 과 label을 같이 가져갑니다 -->
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">게임 개발 유니티</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">어쩌구 개발</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">저쩌구 개발발</label>
+                        </div>
                     </div>
-                    <div>
-                        <input id="one" type="radio" name="answer_1">
-                        <label for="one">저쩌구 개발발</label>
+                    <div class="btn_wrap btn_sort">
+                        <button class="prev_btn">이 전</button>
+                        <button class="next_btn">다 음</button>
                     </div>
                 </div>
-                <div class="button_wrap">
-                    <button class="next_btn">다 음</button>
+                <div class="test">
+                    <div class="question_container">
+                        <h3 class="number">3/-</h3>
+                        <h3 class="question">당신이 가장 재밌었던 수업은?</h3>
+                    </div>
+                    <div class="answer">
+                        <div> <!-- div 1개에 질문 1개 -->
+                            <!-- input 과 label을 같이 가져갑니다 -->
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">게임 개발 유니티</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">어쩌구 개발</label>
+                        </div>
+                        <div>
+                            <input id="one" type="radio" name="answer_1">
+                            <label for="one">저쩌구 개발발</label>
+                        </div>
+                    </div>
+                    <div class="btn_wrap btn_sort">
+                        <button class="prev_btn">이 전</button>
+                        <input type="submit" value="제 출" class="submit_btn">
+                    </div>
                 </div>
             </form>
         </div>
