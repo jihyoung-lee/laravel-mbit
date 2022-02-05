f627b2eea62b6beb9c30e0c84e032fb5600f3c76
지형 <jihyoung.lee90@gmail.com>
2022-02-05 15:29:07 +0900

    Create form.js

---
+++ public/js/form.js
@@ -0,0 +1,25 @@
+function scrollUp(){
+
+}
+
+function scrollDown(){
+
+}
+
+/*
+$(function (){
+    $('.next_btn').click(function (e){
+
+    } // 버튼 클릭 조사하는 코드
+
+    $('.prev_btn').click(function (e){
+
+    }
+
+    $('#form').click(function (e){
+
+    })
+    ))
+})
+
+*/
