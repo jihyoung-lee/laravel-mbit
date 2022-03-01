4e61867b3c51badc9265cc7b1d49cc148ee685a5
지형 <jihyoung.lee90@gmail.com>
2022-03-01 18:16:09 +0900

    Update form.js
    코드 수정

--- public/js/form.js
+++ public/js/form.js
@@ -1,25 +1,27 @@
-function scrollUp(){
-
-}
 
 function scrollDown(){
-
+        const vheight = $('.test').height();
+        $('html, body').animate({
+            scrollTop: ((Math.floor($(window).scrollTop() / vheight)+1) * vheight)
+        }, 500);
 }
-
-/*
+function scrollUp() {
+    const vheight = $('.test').height();
+    $('html, body').animate({
+        scrollTop: ((Math.ceil($(window).scrollTop() / vheight)-1) * vheight)
+    }, 500);
+}
 $(function (){
     $('.next_btn').click(function (e){
+        e.preventDefault(); //btn 클릭 시의 기본 동작 중지
+        scrollDown() //함수 호출
+    });
+    $('.prev_btn').click(function(e){
+        e.preventDefault();
+        scrollUp();
+    });
+});
 
-    } // 버튼 클릭 조사하는 코드
 
-    $('.prev_btn').click(function (e){
 
-    }
 
-    $('#form').click(function (e){
-
-    })
-    ))
-})
-
-*/
