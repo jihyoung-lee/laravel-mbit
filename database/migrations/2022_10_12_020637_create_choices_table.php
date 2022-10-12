261b37052c8f6e0491d8d779122dc77481ca7662
지형 <jihyoung.lee90@gmail.com>
2022-10-12 12:34:23 +0900

    create migration

---
+++ database/migrations/2022_10_12_020637_create_choices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->string('content',100);
+            $table->foreignId('developer')->references('id')->
+            on('developers')->onDelete('cascade');
+            $table->foreignId('question')->nullable()->references('id')->
+            on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choice');
+    }
+}
