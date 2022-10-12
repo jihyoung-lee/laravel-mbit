a853c86ba88223fd6ee62eefde73541458d1cf95
지형 <jihyoung.lee90@gmail.com>
2022-10-12 12:33:47 +0900

    Update MainController.php
    데이터 보내기

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Developer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('main');
+        $data = Developer::all();
+        return view('main',['data'=>$data]);
     }
 }
